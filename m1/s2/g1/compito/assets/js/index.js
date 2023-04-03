27d7cd56a0c7312b1be560723f666ccf3f3fc8c7
FloriiPaul09 <floriipaul@gmail.com>
2023-04-03 15:22:45 +0200

    first time with js

---
+++ m1/s2/g1/compito/assets/js/index.js
@@ -0,0 +1,23 @@
+alert("Hello World!");
+document.getElementById("primo").innerHTML = "Hello World!";
+
+var uno = 6;
+var due = 12;
+console.log(uno + due);
+console.log(uno * due);
+console.log(due - uno);
+console.log(due / uno);
+
+const nomeuomo = 'Andrea';
+const nomedonna = 'Anna';
+console.log(nomeuomo);
+console.log(nomedonna);
+
+function primo() {
+    console.log("Hello World!");
+}
+
+primo();
+
+var $name = 'Luca';
+document.write('Luca');
