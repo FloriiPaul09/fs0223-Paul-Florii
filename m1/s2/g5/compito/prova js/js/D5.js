bfd7226bb64927c3f6c710c8d1a46ea52732549c
FloriiPaul09 <floriipaul@gmail.com>
2023-04-07 11:47:50 +0200

    js es 1 al 8 completati

--- m1/s2/g5/compito/prova js/js/D5.js
+++ m1/s2/g5/compito/prova js/js/D5.js
@@ -94,17 +94,32 @@ REGOLE
         Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
         "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
     */
+    for (i = 0; i < cars.length; i++) {
+        if (cars[i].color.charAt(0) === 'b') {
+            console.log('Fizz');
+        } else {
+            console.log('Buzz');
+        }
+    }
 }
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
-const numericArray = [
-    6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
-]
+{
 
+    const numericArray = [
+        6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
+    ]
+
+
+}
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
     es. [f, b, e] --> [6, 2, 5]
 */
-const charactersArray = ["g", "n", "u", "z", "d"]
+{
+    const charactersArray = ["g", "n", "u", "z", "d"]
+
+
+}
