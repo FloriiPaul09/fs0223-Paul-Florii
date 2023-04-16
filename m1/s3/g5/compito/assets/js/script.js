712575b87b949dfe8a79688534af151ff2883a8e
FloriiPaul09 <floriipaul@gmail.com>
2023-04-16 15:36:36 +0200

    almost complete

--- m1/s3/g5/compito/assets/js/script.js
+++ m1/s3/g5/compito/assets/js/script.js
@@ -202,12 +202,34 @@ REGOLE
 */
 
 {
-
+    let howManyDays = (data) => {
+        let now = new Date().getTime;
+        let diff = now - new Date(data).getTime();
+        let day = Math.floor(diff / (1000 * 60 * 60 * 24));
+        return day;
+    }
+    let day = new Date(2023, 03, 13);
+    console.log(day);
 }
 
 /* ESERCIZIO 10
   Scrivi una funzione chiamata "isTodayMyBirthday" che deve ritornare true se oggi è il tuo compleanno, falso negli altri casi.
 */
+
+{
+    let isTodayMyBirthday = (days, months) => {
+        let today = new Date();
+        let monthDay = today.getMonth() + 1;
+        let day = today.getDate
+
+        if (day === days && monthDay === months) {
+            return console.log(true);
+        } else {
+            return console.log(false);
+        }
+    }
+    isTodayMyBirthday(09, 10);
+}
 
 // Arrays & Oggetti
 
@@ -218,68 +240,330 @@ REGOLE
   in esso la proprietà chiamata come la stringa passata come secondo parametro.
 */
 
+{
+
+
+    let deleteProp = (obj, prop) => {
+        delete obj[prop];
+        return obj;
+    }
+    console.log(deleteProp({
+        parola: 'Non so cosa scrivere',
+        numero: 9,
+        quote: 'A...rise now, ye tarnished'
+    }, "parola"));
+
+}
+
 /* ESERCIZIO 12
   Scrivi una funzione chiamata "newestMovie" che trova il film più recente nell'array "movies" fornito.
 */
 
-/* ESERCIZIO 13
-  Scrivi una funzione chiamata countMovies che ritorna il numero di film contenuti nell'array "movies" fornito.
-*/
+{
+    const movies = [{
+            Title: 'The Lord of the Rings: The Fellowship of the Ring',
+            Year: '2001',
+            imdbID: 'tt0120737',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg',
+        },
 
-/* ESERCIZIO 14
-  Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
-*/
+        {
+            Title: 'The Lord of the Rings: The Return of the King',
+            Year: '2003',
+            imdbID: 'tt0167260',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+        },
+        {
+            Title: 'The Lord of the Rings: The Two Towers',
+            Year: '2002',
+            imdbID: 'tt0167261',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+        },
+        {
+            Title: 'Lord of War',
+            Year: '2005',
+            imdbID: 'tt0399295',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+        },
+        {
+            Title: 'Lords of Dogtown',
+            Year: '2005',
+            imdbID: 'tt0355702',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+        },
+        {
+            Title: 'The Lord of the Rings',
+            Year: '1978',
+            imdbID: 'tt0077869',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+        },
+        {
+            Title: 'Lord of the Flies',
+            Year: '1990',
+            imdbID: 'tt0100054',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg',
+        },
+        {
+            Title: 'The Lords of Salem',
+            Year: '2012',
+            imdbID: 'tt1731697',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg',
+        },
+        {
+            Title: 'Greystoke: The Legend of Tarzan, Lord of the Apes',
+            Year: '1984',
+            imdbID: 'tt0087365',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg',
+        },
+        {
+            Title: 'Lord of the Flies',
+            Year: '1963',
+            imdbID: 'tt0057261',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg',
+        },
+        {
+            Title: 'The Avengers',
+            Year: '2012',
+            imdbID: 'tt0848228',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+        },
+        {
+            Title: 'Avengers: Infinity War',
+            Year: '2018',
+            imdbID: 'tt4154756',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
+        },
+        {
+            Title: 'Avengers: Age of Ultron',
+            Year: '2015',
+            imdbID: 'tt2395427',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',
+        },
+        {
+            Title: 'Avengers: Endgame',
+            Year: '2019',
+            imdbID: 'tt4154796',
+            Type: 'movie',
+            Poster: 'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg',
+        },
+    ]
 
-/* ESERCIZIO 15
-  Scrivi una funzione chiamata "onlyInLastMillennium" che ritorna solamente i film prodotto nel millennio scorso contenuti nell'array "movies" fornito.
-*/
+    let newestMovie = (movies) => {
+        return movies.sort((a, b) => b.Year - a.Year).slice(0, 1);
 
-/* ESERCIZIO 16
-  Scrivi una funzione chiamata "sumAllTheYears" che ritorna la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array "movies" fornito.
-*/
+    }
 
-/* ESERCIZIO 17
-  Scrivi una funzione chiamata "searchByTitle" che riceve una stringa come parametro e ritorna i film nell'array "movies" fornito che la contengono nel titolo.
-*/
+    console.log(newestMovie(movies));
 
-/* ESERCIZIO 18
-  Scrivi una funzione chiamata "searchAndDivide" che riceve una stringa come parametro e ritorna un oggetto contenente due array: "match" e "unmatch".
-  "match" deve includere tutti i film dell'array "movies" fornito che contengono la stringa fornita all'interno del proprio titolo, mentre "unmatch" deve includere tutti i rimanenti.
-*/
 
-/* ESERCIZIO 19
-  Scrivi una funzione chiamata "removeIndex" che riceve un numero come parametro e ritorna l'array "movies" fornito privo dell'elemento nella posizione ricevuta come parametro.
-*/
+    /* ESERCIZIO 13
+      Scrivi una funzione chiamata countMovies che ritorna il numero di film contenuti nell'array "movies" fornito.
+    */
 
+
+    let countMovies = (movies) => {
+
+        let count = movies.reduce((c, movies) => movies.Type === 'movie' ? c + 1 : c, 0);
+        return count;
+    }
+    console.log(countMovies(movies));
+
+
+    /* ESERCIZIO 14
+      Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
+    */
+
+    let onlyTheYears = (movies) => {
+
+        let years = movies.reduce((years, movies) => {
+            if (movies.Type === 'movie') {
+                years.push(movies.Year);
+            }
+            return years;
+        }, []);
+        return years;
+    }
+    console.log(onlyTheYears(movies));
+
+    /* ESERCIZIO 15
+      Scrivi una funzione chiamata "onlyInLastMillennium" che ritorna solamente i film prodotto nel millennio scorso contenuti nell'array "movies" fornito.
+    */
+
+    let onlyInLastMillennium = (movies) => {
+        let moviesLastMillenium = [];
+
+
+    }
+    console.log(onlyInLastMillennium());
+
+    /* ESERCIZIO 16
+    Scrivi una funzione chiamata "sumAllTheYears" che ritorna la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array "movies" fornito.
+    */
+
+    let sumAllTheYears = (movies) => {
+        sum = 0;
+
+        for (let i = 0; i < movies.length; i++) {
+            sum += +movies[i].Year;
+        }
+        return sum;
+    }
+    console.log(sumAllTheYears(movies));
+
+    /* ESERCIZIO 17
+    Scrivi una funzione chiamata "searchByTitle" che riceve una stringa come parametro e ritorna i film nell'array "movies" fornito che la contengono nel titolo.
+    */
+
+    let searchByTitle = (movies, title) => {
+        let result = [];
+        for (let i = 0; i < movies.length; i++) {
+            if (movies[i].Title === title) {
+                result.push(movies[i]);
+            }
+        }
+        return result;
+    }
+    console.log(searchByTitle(movies, 'The Lords of Salem'));
+
+    /* ESERCIZIO 18
+    Scrivi una funzione chiamata "searchAndDivide" che riceve una stringa come parametro e ritorna un oggetto contenente due array: "match" e "unmatch".
+    "match" deve includere tutti i film dell'array "movies" fornito che contengono la stringa fornita all'interno del proprio titolo, mentre "unmatch" deve includere tutti i rimanenti.
+    */
+
+    let searchAndDivide = (movies, title) => {
+        let match = [];
+        let unmatch = [];
+        for (let i = 0; i < movies.length; i++) {
+            if (movies[i].Title === title) {
+                match.push(movies[i]);
+            } else {
+                unmatch.push(movies[i]);
+            }
+        }
+        return {
+            match: match,
+            unmatch: unmatch
+        }
+    }
+    console.log(searchAndDivide(movies, 'The Lords of Salem'));
+
+    /* ESERCIZIO 19
+    Scrivi una funzione chiamata "removeIndex" che riceve un numero come parametro e ritorna l'array "movies" fornito privo dell'elemento nella posizione ricevuta come parametro.
+    */
+
+    let removeIndex = (movies, index) => {
+
+    }
+
+
+
+}
 // DOM (nota: gli elementi che selezionerai non si trovano realmente nella pagina)
 
 /* ESERCIZIO 20
   Scrivi una funzione per selezionare l'elemento dotato di id "container" all'interno della pagina.
 */
-
+{
+    function idSelector() {
+        let container = document.getElementById('container')
+        container.innerHTML = ''
+    }
+    idSelector();
+}
 /* ESERCIZIO 21
   Scrivi una funzione per selezionare ogni tag <td> all'interno della pagina.
 */
+
+{
+    function selectorTable() {
+        let tdTable = document.querySelectorAll('td');
+    }
+    selectorTable();
+}
 
 /* ESERCIZIO 22
   Scrivi una funzione che, tramite un ciclo, stampa in console il testo contenuto in ogni tag <td> all'interno della pagina.
 */
 
+{
+    function containerTable() {
+        let tdTable = document.querySelectorAll('td');
+        for (let i = 0; i < tdTable.length; i++) {
+            console.log(tdTable[i].innerHTML);
+        }
+    }
+    containerTable();
+}
 /* ESERCIZIO 23
   Scrivi una funzione per aggiungere un background di colore rosso a ogni link all'interno della pagina.
 */
+
+{
+    function backgroundTable() {
+        let anchor = document.querySelectorAll('a');
+        for (let i = 0; i < anchor.length; i++) {
+            anchor[i].style.backgroundColor = 'rgb(255, 0, 0)';
+        }
+    }
+    backgroundTable();
+}
 
 /* ESERCIZIO 24
   Scrivi una funzione per aggiungere un nuovo elemento alla lista non ordinata con id "myList".
 */
 
+{
+    function addElement(n) {
+        let list = document.querySelectorAll('li');
+        for (let i = 0; i < list.length; i++) {
+            let li = list[i];
+            li.id = 'myList';
+        }
+
+    }
+    addElement(4);
+}
+
 /* ESERCIZIO 25
   Scrivi una funzione per svuotare la lista non ordinata con id "myList".
 */
 
+{
+    function removeElement(n) {
+        let remove = document.getElementById('myList');
+        for (let i = 0; i < remove.length; i++) {
+            li.parentNode.removeChild(remove[i]);
+        }
+    }
+    removeElement(4);
+}
+
 /* ESERCIZIO 26
   Scrivi una funzione per aggiungere ad ogni tag <tr> la classe CSS "test"
 */
+
+{
+    function addClass() {
+        let tr = document.querySelectorAll('tr');
+        for (let i = 0; i < tr.length; i++) {
+            tr[i].classList.add('test');
+        }
+    }
+    addClass();
+}
 
 // [EXTRA] JS Avanzato
 
@@ -295,6 +579,22 @@ REGOLE
 
 */
 
+{
+    function halfTree(n) {
+        let particle = '';
+        for (let i = 1; i <= n; i++) {
+
+            for (k = 0; k < i; k++) {
+                particle += '*';
+            }
+            particle += '\n';
+        }
+        return particle;
+    }
+    console.log(halfTree(3));
+
+}
+
 /* ESERCIZIO 28
   Crea una funzione chiamata "tree" che riceve un numero come parametro e costruisce un albero di "*" (asterischi) dell'altezza fornita.
 
@@ -306,10 +606,41 @@ REGOLE
   *****
 
 */
+{
+    function tree(n) {
+        let particle = '';
+        for (let i = 1; i <= n; i++) {
+            for (let j = 0; j <= n - i; j++) {
+                particle += ' ';
+            }
+            for (k = 0; k < (i * 2) - 1; k++) {
+                particle += '*';
+            }
+            particle += '\n';
+        }
+        return particle;
+    }
+    console.log(tree(3));
+}
+
+
+
 
 /* ESERCIZIO 29
   Crea una funzione chiamata "isItPrime" che riceve un numero come parametro e ritorna true se il numero fornito è un numero primo.
 */
+
+{
+    function isItPrime(n) {
+        for (let i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    console.log(isItPrime(23));
+}
 
 /* Questo array viene usato per gli esercizi. Non modificarlo. */
 
