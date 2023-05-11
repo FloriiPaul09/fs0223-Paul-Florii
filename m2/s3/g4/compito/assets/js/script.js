407686d8c378e2d4467c0a539c30283b159c6fa3
FloriiPaul09 <floriipaul@gmail.com>
2023-05-11 15:36:48 +0200

    function buttons fetch

--- m2/s3/g4/compito/assets/js/script.js
+++ m2/s3/g4/compito/assets/js/script.js
@@ -1,1 +1,30 @@
-fetch('https://api.github.com/users/jason-williams')
+let keyApi = "KmM9xKD9CXFfYgDvbw07FmAJCaqAfJedXmjvCA0Pdt569IdelcZP7HHo";
+
+
+//function of the button with id "primaryImg" for the fetch of the primary images
+let loadPrimary = function () {
+    let primaryImg = document.getElementById('primaryImg');
+    primaryImg.addEventListener('click', function () {
+        let primary = fetch('https://api.pexels.com/v1/search?query=animals',{
+            method: 'GET',
+            headers: {
+                'Authorization': keyApi
+            }
+        });
+    });
+};
+loadPrimary();
+
+//function of the button with id "secondaryImg" for the fetch of the secondary images
+let loadSecondary = function () {
+    let secondaryImg = document.getElementById('secondaryImg');
+    secondaryImg.addEventListener('click', function () {
+        let secondary = fetch('https://api.pexels.com/v1/search?query=house',{
+            method: 'GET',
+                        headers: {
+                            'Authorization': keyApi
+                        }
+        });
+        })
+    }
+loadSecondary();
