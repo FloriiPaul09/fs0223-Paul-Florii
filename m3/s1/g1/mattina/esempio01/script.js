fe0691b190330cb16c33d6f5a9d28561a35a3116
FloriiPaul09 <floriipaul@gmail.com>
2023-05-22 13:12:24 +0200

    esempi ts

---
+++ m3/s1/g1/mattina/esempio01/script.js
@@ -0,0 +1,16 @@
+console.log('Ciao!');
+var nome = 'Mario';
+var anni = 26;
+var boolean = false;
+var array = [20, false, 'text'];
+var arrayString = ['string'];
+var arrayNumber = [45, 75, 22];
+var empty;
+empty = 45;
+function saluto() {
+    console.log('Ciaoo!');
+}
+saluto();
+function restituisciSaluto() {
+    return 'Ciaoo';
+}
