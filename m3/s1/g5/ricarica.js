19e0cc515d240d3717e2b5a0f2537370631be1af
FloriiPaul09 <floriipaul@gmail.com>
2023-05-26 11:30:47 +0200

    G5

---
+++ m3/s1/g5/ricarica.js
@@ -0,0 +1,67 @@
+// creazione interfaccia "smartphone"
+;
+// creazione classe smartphone che implementa l'interfaccia precedentemente definita'
+var smartphone = /** @class */ (function () {
+    //costruttore variabili
+    function smartphone(carica, numeroChiamate, costoMinuto) {
+        this.carica = carica;
+        this.numeroChiamate = numeroChiamate;
+        this.costoMinuto = costoMinuto;
+    }
+    //definizione metodi:
+    //1. metodo ricarica
+    smartphone.prototype.ricarica = function (euro) {
+        this.carica += euro;
+        console.log("Ricarica di \u20AC ".concat(euro, " eseguita"));
+    };
+    //2. metodo visualizzazione credito attuale
+    smartphone.prototype.numero404 = function () {
+        return "Il credito attuale e di \u20AC ".concat(this.carica);
+    };
+    //3. metodo visualizzazione delle chiamate totali
+    smartphone.prototype.getNumeroChiamate = function () {
+        return this.numeroChiamate;
+    };
+    //4. metodo durata delle chiamate
+    smartphone.prototype.chiamata = function (minuto) {
+        this.costoMinuto = this.costoMinuto * minuto;
+        console.log("Il costo della chiamata effettuata e di \u20AC ".concat(this.costoMinuto));
+    };
+    //5. metodo di azzeramento delle chiamate
+    smartphone.prototype.azzeraChiamate = function () {
+        this.numeroChiamate = 0;
+        console.log("le chiamate sono state azzerate");
+    };
+    return smartphone;
+}());
+;
+//creazione prima istanza della classe
+console.log("Primo Telefono");
+var telefonoUno = new smartphone(18, 54, 0.20);
+//visualizzazione dei metodi a seconda dei dati dell'istanza
+telefonoUno.ricarica(15);
+console.log(telefonoUno.numero404());
+console.log("Le chiamate effettuate sono:", telefonoUno.getNumeroChiamate());
+telefonoUno.chiamata(108);
+telefonoUno.azzeraChiamate();
+console.log(" ");
+//creazione seconda istanza della classe
+console.log("Secondo telefono");
+var telefonoDue = new smartphone(20, 3, 0.15);
+//visualizzazione dei metodi a seconda dei dati dell'istanza
+telefonoDue.ricarica(7);
+console.log(telefonoDue.numero404());
+console.log("Le chiamate effettuate sono:", telefonoDue.getNumeroChiamate());
+telefonoDue.chiamata(200);
+telefonoDue.azzeraChiamate();
+console.log(" ");
+//creazione terza istanza della classe
+console.log("Terzo telefono");
+var telefonoTre = new smartphone(27, 42, 0.30);
+//visualizzazione dei metodi a seconda dei dati dell'istanza
+telefonoTre.ricarica(10);
+console.log(telefonoTre.numero404());
+console.log("Le chiamate effettuate sono:", telefonoTre.getNumeroChiamate());
+telefonoTre.chiamata(34);
+telefonoTre.azzeraChiamate();
+console.log(" ");
---
+++ m3/s1/g5/ricarica.ts
@@ -0,0 +1,107 @@
+// creazione interfaccia "smartphone"
+
+interface Smartphone{
+    //variabili interfaccia "smartphone"
+
+    carica: number; //credito dello smartphone
+    numeroChiamate: number; //chiamate effettuate
+    costoMinuto: number; //costo della chiamata al minuto 
+
+    //metodo della ricarica
+    ricarica(euro:number):void;
+
+    //metodo di visualizzazione del credito disponibile
+    numero404():string;
+
+    //metodo di visualizzazione del numero totale di chiamate
+    getNumeroChiamate():number;
+
+    //metodo di durata della chiamata
+    chiamata(minuto:number):void;
+
+    //metodo di azzeramento chiamate
+    azzeraChiamate():void;
+};
+
+
+// creazione classe smartphone che implementa l'interfaccia precedentemente definita'
+class smartphone implements Smartphone {
+    //richiamo variabili
+    carica: number;
+    numeroChiamate: number;
+    costoMinuto: number;
+
+    //costruttore variabili
+    constructor(carica:number, numeroChiamate: number, costoMinuto: number){
+        this.carica = carica;
+        this.numeroChiamate = numeroChiamate;
+        this.costoMinuto = costoMinuto;
+    }
+    
+    //definizione metodi:
+
+    //1. metodo ricarica
+    ricarica(euro: number): void {
+        this.carica += euro;
+
+        console.log(`Ricarica di € ${euro} eseguita`);
+    }
+
+    //2. metodo visualizzazione credito attuale
+    numero404(): string {
+        return `Il credito attuale e di € ${this.carica}`
+    }
+    
+    //3. metodo visualizzazione delle chiamate totali
+    getNumeroChiamate(): number {
+        return this.numeroChiamate;
+    }
+
+    //4. metodo durata delle chiamate
+    chiamata(minuto: number): void {
+        this.costoMinuto = this.costoMinuto * minuto;
+        console.log(`Il costo della chiamata effettuata e di € ${this.costoMinuto}`);
+    }
+
+    //5. metodo di azzeramento delle chiamate
+    azzeraChiamate(): void {
+        this.numeroChiamate = 0;
+        console.log(`le chiamate sono state azzerate`);
+    }
+};
+
+//creazione prima istanza della classe
+console.log("Primo Telefono");
+let telefonoUno = new smartphone(18, 54, 0.20);
+
+//visualizzazione dei metodi a seconda dei dati dell'istanza
+telefonoUno.ricarica(15);
+console.log(telefonoUno.numero404());
+console.log("Le chiamate effettuate sono:", telefonoUno.getNumeroChiamate());
+telefonoUno.chiamata(108);
+telefonoUno.azzeraChiamate();
+console.log(" ");
+
+//creazione seconda istanza della classe
+console.log("Secondo telefono");
+let telefonoDue = new smartphone(20, 3, 0.15);
+
+//visualizzazione dei metodi a seconda dei dati dell'istanza
+telefonoDue.ricarica(7);
+console.log(telefonoDue.numero404());
+console.log("Le chiamate effettuate sono:", telefonoDue.getNumeroChiamate());
+telefonoDue.chiamata(200);
+telefonoDue.azzeraChiamate();
+console.log(" ");
+
+//creazione terza istanza della classe
+console.log("Terzo telefono");
+let telefonoTre = new smartphone(27, 42, 0.30);
+
+//visualizzazione dei metodi a seconda dei dati dell'istanza
+telefonoTre.ricarica(10);
+console.log(telefonoTre.numero404());
+console.log("Le chiamate effettuate sono:", telefonoTre.getNumeroChiamate());
+telefonoTre.chiamata(34);
+telefonoTre.azzeraChiamate();
+console.log(" ");
