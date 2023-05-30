603ddb6e244303b537e4645a2f19a7ed8af7cf8a
FloriiPaul09 <floriipaul@gmail.com>
2023-05-30 15:44:12 +0200

    completato component nav e aggiunto compnnt main

--- m3/s2/g2/compito/src/app/app.module.ts
+++ m3/s2/g2/compito/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
