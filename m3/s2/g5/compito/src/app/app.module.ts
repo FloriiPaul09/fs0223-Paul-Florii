96af313d301570f1988b7206ac9c667f9fa3975d
FloriiPaul09 <floriipaul@gmail.com>
2023-06-03 18:04:31 +0200

    generati i componenti in comune e le pagine

--- m3/s2/g5/compito/src/app/app.module.ts
+++ m3/s2/g5/compito/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pageComponents/header/header.component';
+import { FooterComponent } from './pageComponents/footer/footer.component';
+import { TodosComponent } from './pages/todos/todos.component';
+import { CompletedComponent } from './pages/completed/completed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TodosComponent,
+    CompletedComponent
   ],
   imports: [
     BrowserModule,
