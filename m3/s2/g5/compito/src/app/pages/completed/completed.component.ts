06b9603ab9eb70547c58b6acc526dda943fad427
FloriiPaul09 <floriipaul@gmail.com>
2023-06-04 18:04:52 +0200

    style header

--- m3/s2/g5/compito/src/app/pages/completed/completed.component.ts
+++ m3/s2/g5/compito/src/app/pages/completed/completed.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/interfaces/todo';
+import { TodoService } from 'src/app/todo.service';
 
 @Component({
   selector: 'app-completed',
   templateUrl: './completed.component.html',
   styleUrls: ['./completed.component.scss']
 })
-export class CompletedComponent {
+export class CompletedComponent implements OnInit {
+  tasks: Todo[] = [];
+  loading: boolean = true;
 
+  constructor(private todoSrvc:TodoService){}
+
+    ngOnInit(){
+      this.getList()
+    }
+
+    delete(id?:number){
+      this.todoSrvc.deleteTask(id)
+      .then(response =>{
+        console.log('Task deleted successfully');
+      })
+    }
+
+    getList(){
+      this.todoSrvc.getList().then(response =>{
+        let todos = response.filter(todo => todo.completed)
+        this.tasks = todos;
+        this.loading = false;
+      })
+    }
+
+    modifyTask(event:Event, todo:Todo){
+      event.preventDefault();
+      this.todoSrvc.updateTask(todo).then(response =>{
+        this.getList();
+      })
+    }
 }
