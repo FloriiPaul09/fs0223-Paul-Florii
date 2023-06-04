f531eb96ec417d71ecd019cf995c6fefee31bdd8
FloriiPaul09 <floriipaul@gmail.com>
2023-06-04 14:04:51 +0200

    todo.service done

--- m3/s2/g5/compito/src/app/todo.service.ts
+++ m3/s2/g5/compito/src/app/todo.service.ts
@@ -8,6 +8,7 @@ export class TodoService {
 
   urlSite:string = "http://localhost:3000/to-do";
 
+  allLists:Todo[]= [];
 
 
   constructor() {}
@@ -15,5 +16,34 @@ export class TodoService {
       return fetch(this.urlSite).then(response => response.json());
     }
 
+    getTodoPromise(){
 
+      return new Promise<Todo[]>((resolve) => {
+        setTimeout(() => {
+          resolve(this.allLists);
+        }, 2000)
+      })
+
+    }
+
+
+    getSingolaTask(id:number):Promise<Todo>{
+      return fetch(this.urlSite+'/'+id).then(response => response.json());
+    }
+
+    addNewTask(task:Todo):Promise<Todo>{
+      return fetch(this.urlSite,{
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(task)
+      }).then(response => response.json());
+    }
+
+    modifyTask(task:Todo){
+      return fetch(this.urlSite + '/' + task.id, {
+        method : 'PUT',
+        headers : {'content-type': 'application/json'},
+        body : JSON.stringify(task)
+      }).then(response => response.json())
+    }
 }
