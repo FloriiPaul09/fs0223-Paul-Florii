c010c25fe438d0e13c7d44ba61daf0401c605a8b
FloriiPaul09 <floriipaul@gmail.com>
2023-06-06 14:51:19 +0200

    first commit giorno 2

---
+++ m3/s3/g2/compito/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateDrivenComponent } from './forms/template-driven/template-driven.component';
+import { ReactiveFormComponent } from './forms/reactive-form/reactive-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateDrivenComponent,
+    ReactiveFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
