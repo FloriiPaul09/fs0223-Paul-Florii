f33147d5295942b9d65d9296f09526eb4d6aed00
FloriiPaul09 <floriipaul@gmail.com>
2023-06-09 16:15:06 +0200

    guard & intercept

--- m3/s3/g5/compito/src/app/auth/auth.guard.ts
+++ m3/s3/g5/compito/src/app/auth/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, map, take } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -16,12 +16,18 @@ export class AuthGuard implements CanActivate, CanActivateChild {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authSrvc.isLoggedIn$.pipe(take(1), map(isLoggedIn => {
+      if (isLoggedIn) {
+        return true;
+      }
+      this.router.navigate(['/auth'])
+      return false;
+    }));
   }
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.canActivate(childRoute, state);
   }
 
 }
