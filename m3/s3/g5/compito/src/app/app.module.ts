84e22f66e14ec38ce9df39a26b8015dd940d694c
FloriiPaul09 <floriipaul@gmail.com>
2023-06-09 12:45:41 +0200

    creati auth files (login e sign up)

--- m3/s3/g5/compito/src/app/app.module.ts
+++ m3/s3/g5/compito/src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SignUpComponent } from './auth/forms/sign-up/sign-up.component';
+import { LogInComponent } from './auth/forms/log-in/log-in.component';
+import { AuthComponent } from './auth/auth.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LogInComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     NgbModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ m3/s3/g5/compito/src/app/auth/interfaces/i-data.ts
@@ -0,0 +1,6 @@
+import { IUser } from './i-user'
+
+export interface IData {
+  accessToken: string;
+  user: IUser;
+}
